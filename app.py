df5e062df569382279ff66d54fd66ff90c345781
LaNgocNam <90526729+LaNgocNam@users.noreply.github.com>
2026-01-17 16:58:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+import yt_dlp
 import os
+import uuid
 
 app = Flask(__name__)
 
+DOWNLOAD_FOLDER = "downloads"
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
 @app.route("/")
 def home():
-    return "WEB DA HOAT DONG ROI 🔥"
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/download", methods=["POST"])
+def download():
+    url = request.form.get("url")
+
+    if not url:
+        return "Chưa nhập link video"
+
+    filename = str(uuid.uuid4()) + ".mp4"
+    filepath = os.path.join(DOWNLOAD_FOLDER, filename)
+
+    ydl_opts = {
+        "outtmpl": filepath,
+        "format": "mp4"
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return
